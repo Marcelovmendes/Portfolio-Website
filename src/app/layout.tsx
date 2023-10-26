82aca17c2907fffe35e633cfddc4330a6dae89fc
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-10-25 21:40:17 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Portfolio - Next.js',
+  description: 'Fullstack Next.js portfolio template with Tailwind CSS and TypeScript',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
